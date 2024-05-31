77b18254663b03d02b228f9275993220f7a357df
Niklas Linnanen <nlinnanen@gmail.com>
2024-05-31 15:11:41 +0300

    Refactor member-role relationship logic in the repositories

--- backend/src/repositories/mod.rs
+++ backend/src/repositories/mod.rs
@@ -4,19 +4,20 @@ use member::MemberRepo;
 pub mod application;
 pub mod member;
 pub mod role;
-pub mod role_member;
 
 #[derive(Clone)]
 pub struct PostgresRepo {
     pub member: MemberRepo,
     pub application: ApplicationRepo,
+    pub role: role::RoleRepo,
 }
 
 impl PostgresRepo {
     pub fn new(pool: sqlx::PgPool) -> Self {
         Self {
             member: MemberRepo { pool: pool.clone() },
-            application: ApplicationRepo { pool: pool },
+            application: ApplicationRepo { pool: pool.clone() },
+            role: role::RoleRepo { pool: pool },
         }
     }
 }
--- backend/src/repositories/role.rs
+++ backend/src/repositories/role.rs
@@ -1,5 +1,9 @@
+use chrono::NaiveDate;
 use serde::Serialize;
 use sqlx::PgPool;
+use uuid::Uuid;
+
+use super::member::Member;
 
 #[derive(Clone)]
 pub struct RoleRepo {
@@ -11,12 +15,21 @@ pub struct Role {
     pub name: String,
 }
 
+#[derive(Debug, Clone)]
+pub struct RoleMember {
+    pub user_id: Uuid,
+    pub role_name: String,
+    pub valid_from: NaiveDate,
+    pub valid_until: Option<NaiveDate>,
+}
+
 impl RoleRepo {
-    pub async fn create_role(&self, role_name: &str) -> Result<Role, sqlx::Error> {
-        let created = sqlx::query_as::<_, Role>("INSERT INTO Role (name) VALUES ($1) RETURNING name")
-            .bind(role_name)
-            .fetch_one(&self.pool)
-            .await?;
+    pub async fn create(&self, role_name: &str) -> Result<Role, sqlx::Error> {
+        let created =
+            sqlx::query_as::<_, Role>("INSERT INTO Role (name) VALUES ($1) RETURNING name")
+                .bind(role_name)
+                .fetch_one(&self.pool)
+                .await?;
         Ok(created)
     }
 
@@ -29,11 +42,109 @@ impl RoleRepo {
         Ok(roles)
     }
 
-    pub async fn delete_role(&self, role_name: &str) -> Result<(), sqlx::Error> {
+    pub async fn delete(&self, role_name: &str) -> Result<(), sqlx::Error> {
         sqlx::query("DELETE FROM Role WHERE name = $1")
             .bind(role_name)
             .execute(&self.pool)
             .await?;
         Ok(())
+    }
+
+    pub async fn create_role_member(
+        &self,
+        user_id: Uuid,
+        role_name: &str,
+        valid_from: NaiveDate,
+        valid_until: Option<NaiveDate>,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+          INSERT INTO RoleMember (user_id, role_name, valid_from, valid_until)
+          VALUES ($1, $2, $3, $4)
+          "#,
+            user_id,
+            role_name,
+            valid_from,
+            valid_until
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn update_valid_until(
+        &self,
+        user_id: Uuid,
+        role_name: &str,
+        valid_from: NaiveDate,
+        new_valid_until: NaiveDate,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+          UPDATE RoleMember
+          SET valid_until = $4
+          WHERE user_id = $1 AND role_name = $2 AND valid_from = $3
+          "#,
+            user_id,
+            role_name,
+            valid_from,
+            new_valid_until
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn delete_role_member(
+        &self,
+        user_id: Uuid,
+        role_name: &str,
+        valid_from: NaiveDate,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+          DELETE FROM RoleMember
+          WHERE user_id = $1 AND role_name = $2 AND valid_from = $3
+          "#,
+            user_id,
+            role_name,
+            valid_from
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn fetch_roles_by_member(
+        &self,
+        user_id: Uuid,
+    ) -> Result<Vec<RoleMember>, sqlx::Error> {
+        let records = sqlx::query_as!(
+            RoleMember,
+            r#"
+          SELECT user_id, role_name, valid_from, valid_until
+          FROM RoleMember
+          WHERE user_id = $1
+          "#,
+            user_id
+        )
+        .fetch_all(&self.pool)
+        .await?;
+        Ok(records)
+    }
+
+    pub async fn fetch_members_by_role(&self, role_name: &str) -> Result<Vec<Member>, sqlx::Error> {
+        let records = sqlx::query_as!(
+            Member,
+            r#"
+          SELECT Member.user_id, first_name, last_name, home_municipality, has_accepted_policies
+          FROM RoleMember JOIN Member ON RoleMember.user_id = Member.user_id
+          WHERE role_name = $1
+          "#,
+            role_name
+        )
+        .fetch_all(&self.pool)
+        .await?;
+        Ok(records)
     }
 }
