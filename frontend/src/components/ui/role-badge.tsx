3f52e7c30681112a8b8dee0a89f74fc64897cb9d
Niklas Linnanen <nlinnanen@gmail.com>
2025-06-21 19:26:01 +0300

    Dont capitalize role name to simplify ui

--- frontend/src/components/ui/role-badge.tsx
+++ frontend/src/components/ui/role-badge.tsx
@@ -1,9 +1,8 @@
-import { capitalizeFirstLetter } from "@/lib/utils";
 import { Badge } from "./badge";
 
 const RoleBadge = ({ role }: { role: string }) => {
   return (
-    <Badge variant={"outline"}>{capitalizeFirstLetter(role)}</Badge>
+    <Badge variant={"outline"}>{role}</Badge>
   );
 }
 
