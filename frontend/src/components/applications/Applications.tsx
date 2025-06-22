db1f1a3cd0beb15a62fcbd6180a1d66fa93a86ea
Niklas Linnanen <nlinnanen@gmail.com>
2025-06-22 11:10:51 +0300

    Add linik to targetable roles in application page

--- frontend/src/components/applications/Applications.tsx
+++ frontend/src/components/applications/Applications.tsx
@@ -6,9 +6,19 @@ import { Badge } from "../ui/badge";
 import { useEffect, useState } from "react";
 import { capitalizeFirstLetter } from "@/lib/utils";
 import { useQueryClient } from "@tanstack/react-query";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { Link } from "react-router-dom";
+import { buttonVariants } from "../ui/button";
 
 const Applications = () => {
-  const [selectedStatus, setSelectedStatus] = useState<string | null>('pending');
+  const [selectedStatus, setSelectedStatus] = useState<string | null>(
+    "pending"
+  );
 
   const queryClient = useQueryClient();
 
@@ -18,7 +28,25 @@ const Applications = () => {
 
   return (
     <div className="space-y-4">
-      <h1 className="text-4xl">Applications</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl">Applications</h1>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                to={"/applications/targetable-roles"}
+                className={buttonVariants({ variant: "outline" })}
+              >
+                Application targetable roles
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              The set of roles and valid until dates that can be applied to in
+              the application form.
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <div className="flex space-x-2">
         {APPLICATION_STATUSES.map((status) => (
           <Badge
